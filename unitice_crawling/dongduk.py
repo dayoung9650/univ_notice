a425f42e3e155cb33b926f1ef8f4a23cfb9ff3e9
dayoung9650 <dayoung9650@gmail.com>
2018-12-12 02:37:27 +0900

    수정# isTop DB 넣기 - 실패

--- unitice_crawling/dongduk.py
+++ unitice_crawling/dongduk.py
@@ -45,42 +45,42 @@ try:
     getMaxSql = "SELECT max(n_idx) FROM notice"
     mycursor.execute(getMaxSql)
     max = mycursor.fetchone()[0]
-    print(max)
-    print(type(max))
 except ValueError as e:
     print(e)
+if(not max):
+    max = 0
+print(max)
 
 all_noti=[]
-list = soup.find("table", {"class": "bbs-list notice-list"}).find_all("tr")
-for list_tr in list:
-    # top 아닌 경우
-    if(list_tr.get('class')== None):
-        noti={}
-        # 각 컬럼 td 순회 
-        for td in ['.td-01','.td-02','.td-03','.td-05','.td-06']:
-            for b in list_tr.select(td):
-                noti[td] = b.text.strip().encode('utf-8')
-                print(type(noti[td]))
-                # 링크는 한번 더 들어가서 끌고나와줌
-                if( td =='.td-03'):
-                    noti['url'] = baseUrl + b.a['href'].encode('utf-8')
-        print("-----------")
-        # 빈 경우 처리
-        if(not noti):
-            continue
-        # idx로 중복 데이터 처리 
-        if(int(noti.get('.td-01')) > max):
-            all_noti.append(noti)
+list = soup.find("tbody").find_all("tr")
 
-    # top인경우 db 저장할지, 서버에서 api로 바로 날려줄지 고민! (자주 바껴서)
-    # else:
+for list_tr in list:
+    noti={}
+    # 각 컬럼 td 순회 
+    for td in ['.td-01','.td-02','.td-03','.td-05','.td-06']:
+        for b in list_tr.select(td):
+            noti[td] = b.text.strip().encode('utf-8')
+            # print(noti)
+            # 링크는 한번 더 들어가서 끌고나와줌
+            if( td =='.td-03'):
+                noti['url'] = baseUrl + b.a['href'].encode('utf-8')
+    # Top notice 인 경우 체크 그러나 값이 없는데 pk라서 들어가질 않는다 -> 해결해야함 ㅠ
+    if(not noti['.td-01']):
+        noti['isTop'] = 1
+        noti['.td-01'] = 0 
+    else: 
+        noti['isTop'] = 0
+    # idx로 중복 데이터 처리 
+    if( (int(noti.get('.td-01')) > max) or (noti.get('isTop') == 1)):
+        print(noti)
+        all_noti.append(noti)
 
 print(all_noti)
 for noti in all_noti:
     if(not noti):
         continue
-    insertSql = "INSERT INTO notice(n_idx, n_dept, n_link, n_title, n_date, n_views) VALUES(%s,%s,%s,%s,%s,%s)"
-    print(noti.get('.td-01'), noti.get('.td-02'), noti.get('url'),noti.get('.td-03'),noti.get('.td-05'),noti.get('.td-06') )
-    mycursor.execute(insertSql, (noti.get('.td-01'), unicode(noti.get('.td-02').decode('utf-8')), unicode(noti.get('url').decode('utf-8')),unicode(noti.get('.td-03').decode('utf-8')),noti.get('.td-05'),noti.get('.td-06') ))
+    insertSql = "INSERT INTO notice(n_idx, n_dept, n_link, n_title, n_date, n_views, n_isTop) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+    # print(noti.get('.td-01'), noti.get('.td-02'), noti.get('url'),noti.get('.td-03'),noti.get('.td-05'),noti.get('.td-06') )
+    mycursor.execute(insertSql, (noti.get('.td-01'), unicode(noti.get('.td-02').decode('utf-8')), unicode(noti.get('url').decode('utf-8')),unicode(noti.get('.td-03').decode('utf-8')),noti.get('.td-05'),noti.get('.td-06'), noti.get('isTop') ))
     mydb.commit()
 mydb.close()
